52218b2858e76fa6274eda5687ef7f0587db285b
Boris Glavic <lordpretzel@gmail.com>
2023-01-16 16:10:01 -0600

    datalog grammar

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,9 +3,9 @@
          (eval .
                (set (make-local-variable 'exec-path) (cons "./node_modules/.bin" exec-path)))
          ;; run and compilation commands for GProM
-         (projectile-project-compilation-cmd . "tree-sitter generate")
-         (projectile-project-test-cmd . "tree-sitter test")
-         (projectile-project-run-cmd . "tree-sitter playground")
-         (projectile-project-install-cmd . "tree-sitter build-wasm")
+         (projectile-project-compilation-cmd . "./node_modules/.bin/tree-sitter generate")
+         (projectile-project-test-cmd . "./node_modules/.bin/tree-sitter test")
+         (projectile-project-run-cmd . "./node_modules/.bin/tree-sitter generate && ./node_modules/.bin/tree-sitter build-wasm && ./node_modules/.bin/tree-sitter playground")
+         (projectile-project-install-cmd . "./node_modules/.bin/tree-sitter build-wasm")
          ))
  )
---
+++ grammar.js
@@ -0,0 +1,147 @@
+const PREC = {
+	comparative: 1,
+	multiplicative: 4,
+	additive: 3,
+	concat: 2,
+	function: 5
+}
+
+module.exports = grammar({
+	name: 'gprom_datalog',
+
+	rules: {
+		// TODO: add the actual grammar rules
+		source_file: $ => repeat($._statement),
+
+		_statement: $ => choice(
+			$.rule,
+			$.fact,
+			$.fd,
+			$.lineage,
+			$.answer_predicate
+		),
+
+		fact: $ => seq(
+			$.rel_atom,
+			$._DOT),
+		
+		rule: $ => seq(field("head",$.rel_atom),
+					   $._IMPL,
+					   field("body", commaSep($._body_atom)),
+					   $._DOT),
+
+		_body_atom: $ => choice(
+			$.rel_atom
+		),
+		
+		rel_atom: $ => seq(
+			$.predicate_name,
+			"(",
+			field("arg", commaSep($._expr)),
+			")"),
+		
+		fd: $ => seq(
+			"FD",
+			$.predicate_name,
+			":",
+			field("LHS", commaSep($.attribute)),
+			"->",
+			field("RHS", commaSep($.attribute)),
+			$._DOT
+		),
+
+		attribute: $ => $._IDENTIFIER,
+				
+		lineage: $ => seq(
+			"LINEAGE",
+			optional(
+				seq(
+					seq("FOR", field("target",$.predicate_name)),
+					optional(
+						seq("FOR", "RESULTS", "FROM", field("result_table",$.predicate_name))
+					)
+				)
+			),
+			$._DOT
+		),
+
+		answer_predicate: $ => seq(
+			"ANS",
+			":",
+			$.predicate_name,
+			$._DOT
+		),
+		
+		_expr: $ => choice(
+			prec.left(PREC.function,$.function_call),
+			$.variable,
+			$.binary_expr,
+			$.constant,
+			seq('(', $._expr, ')')
+		), 		
+
+		function_call: $ => seq(
+			field("name", choice(
+				$.agg_function,
+				$.function_name
+			)),
+			"(",
+			field("arg", commaSep($._expr)),
+			")"
+		),
+
+		function_name: $ => choice(
+			prec(2,$.agg_function),
+			prec(1,$._IDENTIFIER)
+		),
+
+		agg_function: $ => choice("sum","avg","count","min","max"),
+		
+		predicate_name: $ => $._IDENTIFIER,
+		
+		variable: $ => $._IDENTIFIER,
+		
+		binary_expr: $ => {
+			const table = [
+				[PREC.concat, '||'],
+				[PREC.comparative, choice('==', '!=', '<', '<=', '>', '>=')],
+				[PREC.additive, choice('+', '-')],
+				[PREC.multiplicative, choice('*', '/', '%')],
+			];
+
+			return choice(...table.map(([precedence, operator]) => prec.left(precedence, seq(
+				field('left', $._expr),
+				field('operator', operator),
+				field('right', $._expr),
+			))));
+		},
+
+		constant: $ => choice(
+			$.NUMBER,
+			$.STRING
+		),
+
+		NUMBER: $ => choice(
+			seq(optional("-"), $._INTEGER),
+			seq(optional("-"), optional($._INTEGER), '.', $._INTEGER)
+		),
+
+		STRING: $ => /[\'][^\']*[\']/, 
+		
+		_INTEGER: $ => /[0-9]+/,
+		
+		_IMPL: $ => ":-",
+
+		_DOT: $ => /[.]/,
+
+		_IDENTIFIER: $ => /[a-zA-Z_][a-zA-Z_0-9]*/
+	}
+})
+
+function commaSep1(rule) {
+	return seq(rule, repeat(seq(',', rule)))
+}
+
+function commaSep(rule) {
+	return optional(commaSep1(rule))
+}
